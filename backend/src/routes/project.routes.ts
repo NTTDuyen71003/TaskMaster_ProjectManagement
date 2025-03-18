cf23b3fb49827e347f4461d791563b7508cb0fb8
Tinas0603 <phamvutien03@gmail.com>
2025-03-18 19:09:25 +0700

    xong get tất cả project từ workspace

--- backend/src/routes/project.routes.ts
+++ backend/src/routes/project.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import {
     createProjectController,
+    getAllProjectsInWorkspaceController,
 } from "../controllers/project.controller";
 
 const projectRoutes = Router();
 
 projectRoutes.post("/workspace/:workspaceId/create", createProjectController);
+
+projectRoutes.get(
+    "/workspace/:workspaceId/all",
+    getAllProjectsInWorkspaceController
+);
 
 export default projectRoutes;
--- backend/src/services/project.service.ts
+++ backend/src/services/project.service.ts
@@ -1,8 +1,4 @@
-import mongoose from "mongoose";
 import ProjectModel from "../models/project.model";
-import TaskModel from "../models/task.model";
-import { NotFoundException } from "../utils/appError";
-import { TaskStatusEnum } from "../enums/task.enum";
 
 export const createProjectService = async (
     userId: string,
@@ -24,4 +20,30 @@ export const createProjectService = async (
     await project.save();
 
     return { project };
+};
+
+export const getProjectsInWorkspaceService = async (
+    workspaceId: string,
+    pageSize: number,
+    pageNumber: number
+) => {
+    // Tìm tất cả các dự án trong không gian làm việc
+    const totalCount = await ProjectModel.countDocuments({
+        workspace: workspaceId,
+    });
+    // Tính toán số lượng dự án cần bỏ qua
+    const skip = (pageNumber - 1) * pageSize;
+
+    const projects = await ProjectModel.find({
+        workspace: workspaceId,
+    })
+        .skip(skip)
+        .limit(pageSize)
+        .populate("createdBy", "_id name profilePicture -password")
+        // Sắp xếp theo thời gian tạo mới nhất
+        .sort({ createdAt: -1 });
+    // Tính toán tổng số trang, nếu có phần dư thì sẽ thêm 1 trang nữa
+    const totalPages = Math.ceil(totalCount / pageSize);
+
+    return { projects, totalCount, totalPages, skip };
 };
